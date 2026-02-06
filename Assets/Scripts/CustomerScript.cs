b02a3b5af0313f9eb3ed6a07fc3125f036be9cb9
iron5050 <benedikt.ingi.a@gmail.com>
2026-02-06 05:23:30 +0000

    npc navigation error push
    List doesnt work need to find a fix for it

--- Assets/Scripts/CustomerScript.cs
+++ Assets/Scripts/CustomerScript.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
@@ -6,8 +7,13 @@ public class CustomerScript : MonoBehaviour
 {
     //refrence to navmesh agent
     private NavMeshAgent _agent;
-    [SerializeField] private GameObject _Destination;
-    
+
+    //this is what destination the npc is trying to get to
+    [SerializeField] private GameObject Destination;
+
+    //this has an error on the list I will need to fix this
+    //testing the pathfinding as waypoints
+    //[SerializeField] private List<GameObject> Waypoints;
 
     //state for entering shop and leaving shop
     //enum for testing with inspector
@@ -39,18 +45,23 @@ public class CustomerScript : MonoBehaviour
     void Update()
     {
         //set destination, this is designated in inspector
-        _agent.SetDestination(_Destination.transform.position);
+        
 
         switch (currentState)
         {
+            // npc begins in this state
+            // customer navigates to the store counter 
             case NPCBehaviour.EnteringShop:
                 Debug.Log("Customer is entering the shop");
+                _agent.SetDestination(Destination.transform.position);
                 break;
 
+            // customer remains still waiting for the next step/state
             case NPCBehaviour.WatingInShop:
                 Debug.Log("Customer wating for potion");
                 break;
 
+            // customer got what they want and moves to waypoint awayfrom the player. 
             case NPCBehaviour.LeavingShop:
                 Debug.Log("Customer is leaving the shop");
                 break;
@@ -65,8 +76,9 @@ public class CustomerScript : MonoBehaviour
         {
             //this should be moved elsewhere in regards to npc dialogue
             currentState = NPCBehaviour.LeavingShop;
-            //object with tag is destroyd. creates illusion of npc reciving item
+            //object with tag is destroyd. creates illusion of npc reciving item (specifie gameobject othervise destroyed object is still visable)
             Destroy(other.gameObject);
+            Debug.Log("Customer got Potion");
         }
     }
 
