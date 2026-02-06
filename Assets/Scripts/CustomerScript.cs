e812d82abbe70c742092a54589f5994e4e2fe37f
iron5050 <benedikt.ingi.a@gmail.com>
2026-02-06 05:39:30 +0000

    Npc waypoints fixed (no list)
    list was not used instead and array for the transform of the waypoints was used
    [SerializeField] public Transform[] Waypoints;

--- Assets/Scripts/CustomerScript.cs
+++ Assets/Scripts/CustomerScript.cs
@@ -8,12 +8,13 @@ public class CustomerScript : MonoBehaviour
     //refrence to navmesh agent
     private NavMeshAgent _agent;
 
-    //this is what destination the npc is trying to get to
-    [SerializeField] private GameObject Destination;
+    //this is what destination the npc is trying to get to (for testing)
+    //[SerializeField] private GameObject Destination;
 
-    //this has an error on the list I will need to fix this
-    //testing the pathfinding as waypoints
-    //[SerializeField] private List<GameObject> Waypoints;
+
+    //Waypoitns for the npc to navigate towards (what these objects are is designated in the inspector)
+    // [] array is for multiple objects 
+    [SerializeField] public Transform[] Waypoints;
 
     //state for entering shop and leaving shop
     //enum for testing with inspector
@@ -29,7 +30,7 @@ public class CustomerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //hanle for component
+        //handle for component
         _agent = GetComponent<NavMeshAgent>();
         //null check
         if ( _agent == null)
@@ -53,7 +54,8 @@ public class CustomerScript : MonoBehaviour
             // customer navigates to the store counter 
             case NPCBehaviour.EnteringShop:
                 Debug.Log("Customer is entering the shop");
-                _agent.SetDestination(Destination.transform.position);
+                //speed in regards to deltatime is not needed since the nav mesh agent handles the speed (you can change it there)
+                _agent.SetDestination(Waypoints[0].transform.position);
                 break;
 
             // customer remains still waiting for the next step/state
@@ -63,6 +65,7 @@ public class CustomerScript : MonoBehaviour
 
             // customer got what they want and moves to waypoint awayfrom the player. 
             case NPCBehaviour.LeavingShop:
+                _agent.SetDestination(Waypoints[1].transform.position);
                 Debug.Log("Customer is leaving the shop");
                 break;
         }
@@ -72,6 +75,7 @@ public class CustomerScript : MonoBehaviour
     //ontrigger checking for potion that customer requested
     private void OnTriggerEnter(Collider other)
     {
+        //if its a potion procced
         if(other.gameObject.tag == "PotionProduct") 
         {
             //this should be moved elsewhere in regards to npc dialogue
