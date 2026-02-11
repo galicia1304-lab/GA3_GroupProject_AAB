b437cf225f5e870926b2d3c4bb783368ec6953ab
johnklima <klima.de.sa@gmail.com>
2026-02-11 19:40:29 +0100

    jah help us

--- Assets/Scripts/CustomerScript.cs
+++ Assets/Scripts/CustomerScript.cs
@@ -83,23 +83,24 @@ public class CustomerScript : MonoBehaviour
         //refrence to other onyl needs to check for tag and not also gameobject
         if(other.tag == "PotionProduct") 
         {
-            Transform trans = other.transform; 
-            PotionInformation PotionValue = trans.GetComponent<PotionInformation>();
+            Transform potion = other.transform; 
+            //PotionInformation PotionValue = trans.GetComponent<PotionInformation>();
 
             //for debugging
             //also null refrence error here
-            int p = PotionValue.IngridentSelected1;
+            //int p = PotionValue.IngridentSelected1;
 
             //note, fixed spelling (thanks John)
             //null refrence error in line below
-            customerRecivedPotionValue1 = PotionValue.IngridentSelected1;   
-            customerRecivedPotionValue2 = PotionValue.IngridentSelected2;
+            //customerRecivedPotionValue1 = PotionValue.IngridentSelected1;   
+            //customerRecivedPotionValue2 = PotionValue.IngridentSelected2;
 
-            Debug.Log(customerRecivedPotionValue1);
-            Debug.Log(customerRecivedPotionValue2);
+            //Debug.Log(customerRecivedPotionValue1);
+            //Debug.Log(customerRecivedPotionValue2);
 
             //check what main potion ingridient is in the potion
-            if (customerRecivedPotionValue1 == 2)
+            
+            /* if (customerRecivedPotionValue1 == 2)
             {
                 //check waht secondary potion ingridient is in the potion
                 if (customerRecivedPotionValue2 == 0)
@@ -107,18 +108,23 @@ public class CustomerScript : MonoBehaviour
                     Debug.Log("Customer wanted this potion");
                 }
             }
-
+            */
 
             //this should be moved elsewhere in regards to npc dialogue
             currentState = NPCBehaviour.LeavingShop;
 
             //here is the npc taking in the value of the potion which will affect the outcome of the narrative
             //customerPotionValue = PotionValues.GetComponent<ReputationValues>().soldierReputation;
-            Debug.Log("print here waht ingridient numbers are in the potion");
+            //Debug.Log("print here waht ingridient numbers are in the potion");
 
             //object with tag is destroyd. creates illusion of npc reciving item (specifie gameobject othervise destroyed object is still visable)
-            Destroy(other.gameObject);
-            Debug.Log("Customer got Potion");
+            //Destroy(other.gameObject);
+
+           //give the potion as an object
+           potion.parent = transform;  //give the actual potion to the NPC
+           potion.position = Vector3.zero;
+           potion.localPosition = Vector3.zero + Vector3.up * 2.0f;
+           Debug.Log("Customer got Potion, should be hovering over head of npc");
         }
     }
 
