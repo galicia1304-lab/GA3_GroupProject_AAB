b88e42088d19a47e696f02000da335402aa8e893
iron5050 <benedikt.ingi.a@gmail.com>
2026-02-19 17:23:40 +0000

    bird npc script

--- Assets/Scripts/BirdNavigationScript.cs
+++ Assets/Scripts/BirdNavigationScript.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BirdNavigationScript : MonoBehaviour
 {
+    //this scrip copies from customer script
+
+
+    private NavMeshAgent _agent;
+    [SerializeField] public Transform[] Waypoints;
+    [SerializeField] private NPCBehaviour currentState;
+
+    private enum NPCBehaviour
+    {
+        Inactive,
+        DeliveringLetter,
+        ExitingShop
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //handle for component
+        _agent = GetComponent<NavMeshAgent>();
+        //null check
+        if (_agent == null)
+        {
+            Debug.LogError("Nav mesh agent is null");
+        }
+        //set what state npc begins in
+        currentState = NPCBehaviour.Inactive;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState) 
+        { 
+            case NPCBehaviour.Inactive:
+                _agent.isStopped = true; 
+                break;     
+            
+            case NPCBehaviour.DeliveringLetter:
+                _agent.SetDestination(Waypoints[0].transform.position);
+                _agent.isStopped = false; 
+                break;
+
+            case NPCBehaviour.ExitingShop:
+                _agent.SetDestination(Waypoints[1].transform.position);
+                _agent.isStopped = false;
+                break;
+        }
+
+        if (_agent.remainingDistance < 2.0)
+        {
+            _agent.isStopped = true;
+        }
+
+
     }
 }
